f453a196058cdc9e46a12dba60aef69a764993dc
happyEyes <sabatominco@gmail.com>
2015-03-11 10:57:32 +0100

    Aggiunta NuovaClasse

---
+++ src/it/modello/NuovaClasse.java
@@ -0,0 +1,33 @@
+package it.auto.modello;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+public class NuovaClasse {
+
+    
+    
+    private String nome;
+    private String cognome;
+    
+    public NuovaClasse(){}
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+}
